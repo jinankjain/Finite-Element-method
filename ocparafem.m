5fe8ea928010f1aa8ae00ae233ffdafdf334418d
Jinank Jain <ug201210017@iitj.ac.in>
2014-06-17 11:19:28 +0530

    Order of convergence calculation

---
+++ ocparafem.m
@@ -0,0 +1,14 @@
+function [maxL2e, L2H1e] = ocparafem(N,c4n,n4e,n4sDb,T,dt,U)
+
+C = zeros(N+1,1);
+D = zeros(N+1,1);
+D1 = zeros(N,1);
+for l = 1:N+1
+   [C(l,1), D(l,1)] = Errj(c4n, n4e, U(:,l), l, dt);
+end
+D;
+D1(1:N,1) = D(1:N,1);
+maxL2e = max(C);
+L2H1e = sqrt(sum(dt.*D1.^2));
+
+
